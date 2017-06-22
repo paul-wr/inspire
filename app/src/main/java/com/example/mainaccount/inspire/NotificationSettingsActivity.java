7c06f681230def22e3da5736424b94e0b9f6f0a2
paul-wr <paul.w35@live.com>
2017-06-22 23:44:26 +0100

    code tidied up, and comments added

--- app/src/main/java/com/example/mainaccount/inspire/NotificationSettingsActivity.java
+++ app/src/main/java/com/example/mainaccount/inspire/NotificationSettingsActivity.java
@@ -1,5 +1,12 @@
 package com.example.mainaccount.inspire;
 
+/**
+ *  Classname: NotificationSettingsActivity.java
+ *  Version 1
+ *  Date: 22 Jun 2017
+ *  @author Paul Wrenn, x15020029
+ */
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -14,12 +21,12 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class NotificationSettingsActivity extends AppCompatActivity {
-    private PendingIntent pendingIntent;
+    private PendingIntent pendingIntent; // PendingIntent stores Intent until Alarm fires
     private int notificationId = 1;
-    public static int userMinutes;
-    public static int userHour;
+    public static int userMinutes; // user specified minute count
+    public static int userHour; // user specified hour of day
     Calendar calendar;
-    TextView minuteView, hourView;
+    TextView minuteView, hourView; // TextViews for each NumberPicker (hour/minute)
 
 
 
@@ -28,25 +35,30 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_settings);
 
+        // calender instance set to default notification time of 18:00
         calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
+        calendar.set(Calendar.HOUR_OF_DAY, 18);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
 
-          /* Retrieve a PendingIntent that will perform a broadcast */
+        // Intent to begin BroadcastReceiver
         Intent alarmIntent = new Intent(NotificationSettingsActivity.this, MyReceiver.class);
+        // PendingIntent holds Intent until called by AlarmManger
         pendingIntent = PendingIntent.getBroadcast(NotificationSettingsActivity.this, 0, alarmIntent, 0);
 
         minuteView = (TextView)findViewById(R.id.minute_view);
+        // NumberPicker object for setting user defined minute value
         NumberPicker minutePicker = (NumberPicker) findViewById(R.id.minute_picker);
         minutePicker.setMaxValue(60);
         minutePicker.setMinValue(0);
         minutePicker.setWrapSelectorWheel(true);
+        // setOnValueChangedListener records change in NumberPicker wheel value
         minutePicker.setOnValueChangedListener( new NumberPicker.
                 OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int
                     oldVal, int newVal) {
+                // set newVal from user to userMinute
                 userMinutes = newVal;
                 minuteView.setText("Selected minute is "+
                         newVal);
@@ -54,22 +66,27 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         });
 
         hourView = (TextView)findViewById(R.id.hour_view);
+        // NumberPicker object for setting user defined hour value
         NumberPicker hourPicker = (NumberPicker) findViewById(R.id.hour_picker);
         hourPicker.setMaxValue(23);
         hourPicker.setMinValue(0);
         hourPicker.setWrapSelectorWheel(true);
+        // setOnValueChangedListener records change in NumberPicker wheel value
         hourPicker.setOnValueChangedListener( new NumberPicker.
                 OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int
                     oldVal, int newVal) {
+                // set newVal from user to userHour
                 userHour = newVal;
                 hourView.setText("Selected hour is "+
                         newVal);
             }
         });
 
-
+        /* set_time button sets values to calender variables and calls start()
+        method to begin notifications at specified time
+        */
         findViewById(R.id.set_time).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,15 +98,16 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         });
 
 
-
-        findViewById(R.id.startAlarm).setOnClickListener(new View.OnClickListener() {
+        // button onClick starts Notifications
+        findViewById(R.id.start_notifications).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 start();
             }
         });
 
-        findViewById(R.id.stopAlarm).setOnClickListener(new View.OnClickListener() {
+        // button onClick stops Notifications
+        findViewById(R.id.stop_notifications).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 cancel();
@@ -101,14 +119,15 @@ public class NotificationSettingsActivity extends AppCompatActivity {
 
 
 
-
+    // start() method defines an AlarmManger to start notifications for set time and launches Intent via PendingIntent
     public void start() {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
-        manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES , pendingIntent);
+        manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_FIFTEEN_MINUTES , pendingIntent);
         Toast.makeText(this, "Notifications activated!", Toast.LENGTH_SHORT).show();
     }
 
+    // cancel() method stops the notifications
     public void cancel() {
         AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         manager.cancel(pendingIntent);
