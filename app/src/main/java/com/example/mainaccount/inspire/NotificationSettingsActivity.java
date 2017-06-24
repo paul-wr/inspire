c215da70414c44b46e7fedc87869b8b3543dfa58
paul-wr <paul.w35@live.com>
2017-06-24 23:32:16 +0100

    clock added

--- app/src/main/java/com/example/mainaccount/inspire/NotificationSettingsActivity.java
+++ app/src/main/java/com/example/mainaccount/inspire/NotificationSettingsActivity.java
@@ -14,8 +14,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -26,7 +26,8 @@ public class NotificationSettingsActivity extends AppCompatActivity {
     public static int userMinutes; // user specified minute count
     public static int userHour; // user specified hour of day
     Calendar calendar;
-    TextView minuteView, hourView; // TextViews for each NumberPicker (hour/minute)
+    TextView clockText; // TextViews for each NumberPicker (hour/minute)
+    TimePicker timePicker;
 
 
 
@@ -34,6 +35,8 @@ public class NotificationSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_settings);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        clockText = (TextView) findViewById(R.id.clock_text);
 
         // calender instance set to default notification time of 18:00
         calendar = Calendar.getInstance();
@@ -46,43 +49,19 @@ public class NotificationSettingsActivity extends AppCompatActivity {
         // PendingIntent holds Intent until called by AlarmManger
         pendingIntent = PendingIntent.getBroadcast(NotificationSettingsActivity.this, 0, alarmIntent, 0);
 
-        minuteView = (TextView)findViewById(R.id.minute_view);
-        // NumberPicker object for setting user defined minute value
-        NumberPicker minutePicker = (NumberPicker) findViewById(R.id.minute_picker);
-        minutePicker.setMaxValue(60);
-        minutePicker.setMinValue(0);
-        minutePicker.setWrapSelectorWheel(true);
-        // setOnValueChangedListener records change in NumberPicker wheel value
-        minutePicker.setOnValueChangedListener( new NumberPicker.
-                OnValueChangeListener() {
+
+        timePicker.setIs24HourView(false);
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
-            public void onValueChange(NumberPicker picker, int
-                    oldVal, int newVal) {
-                // set newVal from user to userMinute
-                userMinutes = newVal;
-                minuteView.setText("Selected minute is "+
-                        newVal);
+            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+                updateDisplay(hour, minute);
             }
+
         });
 
-        hourView = (TextView)findViewById(R.id.hour_view);
-        // NumberPicker object for setting user defined hour value
-        NumberPicker hourPicker = (NumberPicker) findViewById(R.id.hour_picker);
-        hourPicker.setMaxValue(23);
-        hourPicker.setMinValue(0);
-        hourPicker.setWrapSelectorWheel(true);
-        // setOnValueChangedListener records change in NumberPicker wheel value
-        hourPicker.setOnValueChangedListener( new NumberPicker.
-                OnValueChangeListener() {
-            @Override
-            public void onValueChange(NumberPicker picker, int
-                    oldVal, int newVal) {
-                // set newVal from user to userHour
-                userHour = newVal;
-                hourView.setText("Selected hour is "+
-                        newVal);
-            }
-        });
+
+
 
         /* set_time button sets values to calender variables and calls start()
         method to begin notifications at specified time
@@ -114,6 +93,11 @@ public class NotificationSettingsActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    private void updateDisplay(int hourOfDay, int minute) {
+        clockText.setText(hourOfDay+" "+minute);
 
     }
 
